14904be08a6f9041296e49adc2f8ead916375e64
Ben Stein <ben@steinbox.com>
2017-03-19 17:23:12 -0400

    fix report links

--- Achievements/appengine/js/camper-achievement-index.js
+++ Achievements/appengine/js/camper-achievement-index.js
@@ -4,8 +4,8 @@ ca.i = ca.i || {};
 ca.i.navigateReport = function() {
 }
 
-ca.sa.init = function() {
+ca.i.init = function() {
     $('.reportLink').click((event) => {
         location.href = $(event.delegateTarget).data('url') + '?session=' + $('#sessionList').val();
-    }
+    });
 };
